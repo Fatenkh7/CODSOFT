9232ab693cda00f1fba17e66c297689e6fc76c38
Fatenkh7 <khodorfaten5@gmail.com>
2023-09-16 17:43:28 +0300

    remove cors

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
@@ -15,7 +14,6 @@ const PORT = process.env.PORT || 5000
 
 const app = express();
 app.use(express.json());
-app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
