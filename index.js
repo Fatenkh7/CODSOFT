237907f9f729f5dfd80b2ad54dc948f25827e66d
Fatenkh7 <khodorfaten5@gmail.com>
2023-09-16 19:08:15 +0300

    edit the cors

--- index.js
+++ index.js
@@ -15,11 +15,7 @@ const PORT = process.env.PORT || 5000
 
 const app = express();
 app.use(express.json());
-const corsOptions = {
-    optionsSuccessStatus: 200,
-    credentials: true,
-  };
-  app.use(cors(corsOptions));
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
