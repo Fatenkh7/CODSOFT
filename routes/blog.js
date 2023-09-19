daea29bf9510dcb5e33042176cf4ac464e221ead
Fatenkh7 <khodorfaten5@gmail.com>
2023-09-19 23:32:38 +0300

    edit the blog route

--- routes/blog.js
+++ routes/blog.js
@@ -7,7 +7,7 @@ import { add, deleteBlog, getAll, getById, put } from "../controllers/blog.js";
 router.get("/", auth, getAll)
 router.get("/:ID", auth, getById)
 router.put("/:ID", auth, imageHandel, put)
-router.delete("/:ID", auth, imageHandel, deleteBlog)
+router.delete("/:ID", auth, deleteBlog)
 router.post("/add", auth, imageHandel, add);
 
 export default router;
